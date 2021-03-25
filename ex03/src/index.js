e44080fb058e64ace2df12975573b9c14daff3c1
Dzevad <ynwa886@gmail.com>
2021-03-25 22:19:55 +0100

    Other Iterable Objects

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+function* myGenerator() {
+    yield* [1, 2, 3, 4, 5];
+    yield* 'Arena';
+    yield* arguments;
+}
+
+const iterator = myGenerator(6, 7, 8);
+
+const generatorArray = [];
+
+for (let i of myGenerator(6, 7, 8)) {
+    generatorArray.push(i);
+}
+console.log(generatorArray);
+
+module.exports = { generatorArray, myGenerator };
