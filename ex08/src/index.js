3e3ec33eeea9f19d6b4e124b58f437cfeb9a8f8c
Dzevad <ynwa886@gmail.com>
2021-03-26 02:50:22 +0100

    Map and Filter

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -3,8 +3,10 @@ let two = new Set([6, 4, 3, 2]);
 
 unionSet = new Set([...one, ...two]);
 console.log(unionSet);
-intersectionSet = new Set([...one].map());
-differenceSet = new Set([]);
+intersectionSet = new Set([...one].filter( i => two.has(i)));
+console.log(intersectionSet);
+differenceSet = new Set([...one].filter(i => !two.has(i)));
+console.log(differenceSet);
 
 
 module.exports = { unionSet, intersectionSet, differenceSet };
